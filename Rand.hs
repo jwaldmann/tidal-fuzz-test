168dbf11a988a529a7c55014bbe1a6b4bded10a3
Johannes Waldmann <@>
2020-04-22 21:18:20 +0200

    add test for rand

---
+++ Rand.hs
@@ -0,0 +1,25 @@
+{-# language LambdaCase #-}
+{-# language OverloadedStrings #-}
+
+import System.Environment
+import System.IO
+import Sound.Tidal.Context
+
+main = do
+  hSetBuffering stdout NoBuffering
+  getArgs >>= \ case
+    [] -> main_for 10
+    [s] -> main_for  $ read s
+
+-- see https://github.com/tidalcycles/Tidal/issues/636
+
+main_for :: Int -> IO ()
+main_for i = print
+  $ foldr ($) (speed (run 16))
+  $ replicate i
+  $ sometimes (# speed 0)
+
+main_for_1 i = print -- works
+  $ foldr ($) (run 16)
+  $ replicate i
+  $ sometimes (const 0)
--- src/Sound/Tidal/PrintBP.hs
+++ src/Sound/Tidal/PrintBP.hs
@@ -13,6 +13,7 @@ import Sound.Tidal.ParseBP
 import Test.LeanCheck
 import Test.LeanCheck.Generic
 import GHC.Generics
+import Data.Ratio
 
 deriving instance Eq a => Eq (TPat a)
 deriving instance Generic (TPat a)
@@ -27,26 +28,31 @@ p1
   . (Parseable a, Eq a,Printable a, Show a)
   => TPat a
   -> Bool
-p1 p = case parseTPat (printTPat p) of
+p1 p = case parseTPat (print p) of
   Left e  -> error $ unlines
-    [ "could not parse", show p,  printTPat p ]
+    [ "could not parse", show p,  print p ]
   Right q -> normalize p ==  normalize q ||
     error ( unlines
-      ["different values", show p, printTPat p, show q] )
+      ["different values", show p, print p, show q] )
 
 class Printable a where print :: a -> String
 instance Printable Bool where
   print False = "f" ; print True = "t"
+instance Printable Integer where print = show
+instance Printable a => Printable (Ratio a) where
+  print q = print (numerator q) <> "%" <> print (denominator q)
 
+-- | remove source span info in atoms, and some other cleanup
 normalize :: TPat a -> TPat a
-normalize (TPat_Seq [p]) = p
+normalize (TPat_Seq [p]) = normalize p
+normalize (TPat_Atom _ x) = TPat_Atom Nothing x
 normalize p = p
 
-printTPat :: Printable a => TPat a -> String
-printTPat = \ case
+instance Printable a => Printable (TPat a) where
+ print = \ case
   --    TPat_Atom (Maybe ((Int, Int), (Int, Int))) a
   TPat_Atom m x -> print x
-  --  | TPat_Fast (TPat Time) (TPat a)
+  TPat_Fast t p -> print p <> "*" <> print t 
   --  | TPat_Slow (TPat Time) (TPat a)
   --  | TPat_DegradeBy Int Double (TPat a)
   --  | TPat_CycleChoose Int [TPat a]
